0865d5b91d94c63d21f3b4505a4c6b710e756536
victormatosx <victormatos@unipam.edu.br>
2024-05-17 15:15:59 -0300

    .

--- testes/testes.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.querySelector('.navbar-toggle');
-const navbarLinks = document.querySelector('.navbar-links');
-
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('show');
-});
