7fb1d4894c8a3533c391150a7e0015a82e11939a
victormatosx <victormatos@unipam.edu.br>
2024-05-30 19:14:13 -0300

    .

--- cadastro-page copy/config.php
+++ cadastro-page copy/config.php
@@ -1,17 +1,33 @@
 <?php
 
-    $dbHost = 'Localhost';
-    $dbUsername = 'root';
-    $dbPassword = '';
-    $dbName = 'ordermilk';
-
-    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-
-    if($conexao->connect_errno){
-        echo "Erro: ";
+    if(isset($_POST['cadastrar'])){
+        $brinco = $_POST['brinco'];
+        $nome = $_POST['nomeAnimal'];
+        $idade = $_POST['idade'];
+        $lote = $_POST['lote'];
+        $pai = $_POST['pai'];
+        $mae = $_POST['mae'];
+        $crias = $_POST['crias'];
     }
-    else{
-        echo "Conectado";
+
+    $host = 'localhost';
+    $usuario = 'root';
+    $pass = '';
+    $banco = 'ordermilk';
+
+    $con = mysqli_connect($host, $usuario, $pass, $banco);
+
+    if(!$con){
+        die("Falha ao conectar ao banco de dados." . mysqli_connect_error());
+    }
+
+    $sql = "INSERT INTO cadastro_animal (nº_brinco, nome, id_lote, idade, crias, pai, mae) 
+    VALUES('$brinco','$nome', '$idade', '$lote', '$pai', '$mae', '$crias')";
+
+    $rs = mysqli_query($con, $sql);
+
+    if($rs){
+        echo "Usuário cadastrado com sucesso.";
     }
 
 ?>
