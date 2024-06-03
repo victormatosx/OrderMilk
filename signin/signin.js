29d5d3ec6ad21aa210630afe9c05d066f0f487e3
LUC4SLIM4 <lucasvlima@unipam.edu.br>
2024-06-03 15:41:08 -0300

    pesquisar

---
+++ signin/signin.js
@@ -0,0 +1,24 @@
+if (localStorage.getItem("token") == null) {
+    function alerta(){
+        Swal.fire({
+            title: "Você deseja sair?",
+            text: "Você não poderá reverter isto!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Sim, quero sair!",
+            cancelButtonText: "Cancelar"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                title: "Saindo!",
+                text: "Sessão Finalizada.",
+                icon: "success"
+              });
+            }
+          });
+    }
+    window.location.href = "../home-page/home.html";
+  }
+  
